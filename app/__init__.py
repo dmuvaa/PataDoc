87aa07c78da0b837c244ae664293bef735bcf0af
Dennis Muvaa <dmuvaa70@gmail.com>
2024-01-18 17:53:52 +0300

    Deploy Changes

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ import redis
 db = SQLAlchemy()
 login_manager = LoginManager()
 UPLOAD_FOLDER = 'static/uploads/'
-redis_client = redis.Redis.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
+redis_client = redis.Redis.from_url(os.getenv('REDIS_URL', 'redis://red-cmkjj6gl5elc73b8gmsg:6379/0'))
 
 def create_app():
     load_dotenv()
